5994fb125ef17f986faee5bc5dc6dc604d73ca7d
b.chandon <b.chandon@autobiz.com>
2019-09-02 09:10:58 +0200

    WIP

--- src/App.js
+++ src/App.js
@@ -5,18 +5,44 @@ import LoginView from "./views/LoginView.js";
 import RecordsView from "./views/RecordsView.js";
 import RecordsListView from "./views/RecordsListView.js";
 import { QueryParamProvider } from "use-query-params";
-import Amplify, { Auth } from 'aws-amplify';
-import { withAuthenticator } from 'aws-amplify-react';
-import awsconfig from "./aws-config"
+import Amplify, { Auth, API } from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react";
+import awsconfig from "./aws-config";
+
+
+
+Amplify.configure(awsconfig);
 
 function App() {
   Auth.currentAuthenticatedUser().then(user => {
-    console.log("currentAuthenticatedUser",user)
-    Auth.federatedSignIn()
+    console.log("currentAuthenticatedUser", user);
+    //Auth.federatedSignIn()
   });
-  Auth.currentCredentials().then(creds => console.log("Current credentials",creds, "session token", creds.sessionToken));
-  Auth.currentSession().then(session => console.log("token", `Bearer ${session.getAccessToken().getJwtToken()}`))
+  Auth.currentCredentials().then(creds =>
+    console.log(
+      "Current credentials",
+      creds,
+      "session token",
+      creds.sessionToken
+    )
+  );
+  Auth.currentSession().then(session =>
+    console.log("token", `Bearer ${session.getAccessToken().getJwtToken()}`)
+  );
 
+  try {
+    API.get("b2bPlateform", `/filters`).then(result =>
+      console.log("result call", result)
+    );
+  } catch (e) {
+    console.log("error call api", e);
+  }
+
+  API.get("b2bPlateform", "/auction", {
+    queryStringParameters: {
+      id: "018d1b20-ca2b-11e9-940b-a333546ade96"
+    }
+  }).then(result => console.log("result call", result));
 
   return (
     <BrowserRouter>
@@ -35,5 +61,3 @@ function App() {
 
 export default withAuthenticator(App, true);
 
-
-Amplify.configure(awsconfig);
---
+++ src/aws-config.js
@@ -0,0 +1,61 @@
+import { Auth } from 'aws-amplify';
+
+
+const config = {
+    Auth: {
+
+        // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+        identityPoolId: 'eu-west-1:c7ed857d-d1cd-4459-8329-3b146d3738f1',
+        
+        // REQUIRED - Amazon Cognito Region
+        region: 'eu-west-1',
+
+        // OPTIONAL - Amazon Cognito Federated Identity Pool Region 
+        // Required only if it's different from Amazon Cognito Region
+        identityPoolRegion: 'eu-west-1',
+
+        // OPTIONAL - Amazon Cognito User Pool ID
+        userPoolId: 'eu-west-1_YYMUabLh6',
+
+        // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+        userPoolWebClientId: 'o9vmmt4oco9kiju1nk0fqacf',
+
+        // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+        mandatorySignIn: false,
+
+        // OPTIONAL - Configuration for cookie storage
+        // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
+        // cookieStorage: {
+        // // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+        //     domain: '.yourdomain.com',
+        // // OPTIONAL - Cookie path
+        //     path: '/',
+        // // OPTIONAL - Cookie expiration in days
+        //     expires: 365,
+        // // OPTIONAL - Cookie secure flag
+        // // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
+        //     secure: true
+        // },
+
+        // OPTIONAL - customized storage object
+        //storage: new MyStorage(),
+        
+        // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
+        //authenticationFlowType: 'USER_PASSWORD_AUTH'
+    },
+    API: {
+        endpoints: [
+            {
+                name: "b2bPlateform",
+                region : 'eu-west-1',
+                endpoint: "https://rnmjiufyvb.execute-api.eu-west-1.amazonaws.com/TEST",
+                // custom_header: async () => { 
+                //     //return { Authorization : 'token' } 
+                //     // Alternatively, with Cognito User Pools use this:
+                //     return { Authorization: `Bearer ${(await Auth.currentSession()).getAccessToken().getJwtToken()}` }}
+            }
+        ]
+    }
+}
+
+export default config
