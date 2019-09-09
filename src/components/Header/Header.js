5a979ca7aa65f5f5b465b9af28aced0bc560a05d
b.chandon <b.chandon@autobiz.com>
2019-09-09 10:56:20 +0200

    fix header re-render

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,41 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/img/autobiz-marketplace.png";
 import { Auth } from "aws-amplify";
-import { Spinner } from 'reactstrap';
-
+import { Spinner } from "reactstrap";
+import { t } from "../common/Translate"
 
 const Header = () => {
   const [username, setUsername] = useState("");
 
-  Auth.currentUserInfo().then(user => {
-    setUsername(user.username);
-  });
-
+  useEffect(()=>{
+    Auth.currentUserInfo().then(user => {
+      setUsername(user.username);
+    });
+  }, [])
+  
   const signOut = async function() {
-    Auth.signOut()
-      .then(data => console.log(data))
-      .catch(err => console.log(err));
+    await Auth.signOut()
     return;
   };
 
   return (
     <header>
       <Link to="/">
-        <img alt="Autobiz Market" className="logo" src={logo} />
+        <img alt="autobizTrade" className="logo" src={logo} />
       </Link>
       <div className=" float-right mt-1 mr-2">
-
-        { username && <span className="header-username mr-1">Hello {username}</span> || <Spinner className="mr-2" color="primary" size="sm" /> }
-
-        
+        {(username && (
+          <span className="header-username mr-1">{`${t("hello")} ${username}`}</span>
+        )) || <Spinner className="mr-2" color="primary" size="sm" />}
 
         <button
           class="btn btn-outline-success"
           type="submit"
           onClick={() => signOut()}
         >
-          Déconnexion
+          {t("logout")}
         </button>
       </div>
     </header>
