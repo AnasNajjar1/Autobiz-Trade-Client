9a85726acaaee9cf949af9d928d7f87c82ef3d75
Marceau Azzopardi <marceau.azzopardi@protonmail.com>
2019-09-12 15:56:27 +0200

    new logo autobiz Trade

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../assets/img/autobiz-marketplace.png";
+import logo from "../../assets/img/autobiz-trade.svg";
 const Header = () => {
   return (
     <header>
