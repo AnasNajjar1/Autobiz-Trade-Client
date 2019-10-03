87e10c4b5fb30425c3bceef52bbd304c1b07a390
b.chandon <b.chandon@autobiz.com>
2019-10-03 14:55:49 +0200

    modifs config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const ENV = process.env.REACT_APP_ENV;
 
 const apisB2bPlateform = {
-  dev: "http://localhost:3000",
-  staging: "https://88ub06z9zb.execute-api.us-east-1.amazonaws.com/dev"
+  dev: "http://localhost:4000",
+  staging: "https://sskra8rrdj.execute-api.eu-west-1.amazonaws.com/dev"
 };
 exports.apiB2bPlateform = apisB2bPlateform[ENV];
 
