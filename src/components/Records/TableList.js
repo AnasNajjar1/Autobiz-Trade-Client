c86783e409ff663a8c51fb00d75e919ec58eec50
Marceau <3538880-marceau.a@users.noreply.gitlab.com>
2019-10-22 17:51:42 +0200

    format date in front vehicle details

--- src/components/Records/TableList.js
+++ src/components/Records/TableList.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Row } from "reactstrap";
 import { t } from "../common/Translate";
 import _ from "lodash";
-
-function showableValue(value) {
+import moment from "moment";
+import Cookies from "js-cookie";
+const appLanguage = Cookies.get("appLanguage");
+function showableValue(key, value) {
   if (value === null || value === "") return false;
   if (typeof value === "object" && _.isEmpty(value)) return false;
+  if (key === "fiscal" && appLanguage !== "fr") return false;
   return true;
 }
 
@@ -20,6 +23,11 @@ const ListTable = ({ items }) => {
       case "b2cMarketValue":
       case "standardMileage":
         return parseInt(value).toLocaleString();
+      case "gcDate":
+        return moment(value).format("DD-MM-YYYY");
+      case "lastServicingDate":
+      case "nextTechnicalCheckDate":
+        return moment(value).format("MM-YYYY");
       case "fiscal":
         return `${value} CV`;
       case "power":
@@ -57,7 +65,7 @@ const ListTable = ({ items }) => {
       <Row>
         {Object.keys(items).map(
           key =>
-            showableValue(items[key]) && (
+            showableValue(key, items[key]) && (
               <React.Fragment key={key}>
                 <div className="cell">
                   <div className="item">
