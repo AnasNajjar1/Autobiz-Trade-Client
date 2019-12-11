674e851efc54854e4930c1949d0b40b80c60f127
b.chandon <b.chandon@autobiz.com>
2019-12-11 17:11:19 +0100

    modifs bruno

--- src/components/Records/TableList.js
+++ src/components/Records/TableList.js
@@ -1,82 +1,50 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Row } from "reactstrap";
 import { t } from "../common/Translate";
 import _ from "lodash";
 import moment from "moment";
 import Cookies from "js-cookie";
-let gcDate
+let gcDate;
+
+//remove uncomplete data or unwanted data
 function showableValue(key, value, lang) {
-  if (value === null || value === "") return false;
+  if (value === undefined || value === null || value === "") return false;
   if (typeof value === "object" && _.isEmpty(value)) return false;
   if (key === "fiscal" && lang !== "fr") return false;
+  if (key === "origin") return false;
   if (key === "power" && renderValue("power", value, lang) === "") return false;
-  if (key === "b2cMarketValue" && value < 200) return false
-  if (key === "MarketLink") return false
+  if (key === "b2cMarketValue" && value < 200) return false;
+  //if (key === "MarketLink") return false
   return true;
-}
-
-const calculateOwnerShipDuration = (res) => {
-  let years = _.get(res._data, 'years', null)
-  let months = _.get(res._data, 'months', null)
-  let days =  _.get(res._data, 'days', null)
-
-  let nYears = null
-  let nMonths = null
-
-  let durations = {
-    days : days,
-    months : nMonths ? nMonths : months,
-    years : nYears ? nYears : years
-  }
-
-  let duration = Object.entries(durations).map(([key, value]) => {
-    if(key === "days" && value !== null && value > 20) nMonths = months+1
-
-    if(key === 'months' && value !== null && value !== 0) {
-      if(nMonths !== null && (nMonths === 11 || nMonths == 12)) nYears = years+1
-      else if (nMonths !== null && nMonths < 11) return `${nMonths} ${t(key)} `
-      else if (nMonths === null) return `${value} ${t(key)} `
-    }
-
-    if(key === 'years' && value !== null && value !== 0) {
-      if(nYears !== null && nYears === 1) return `${nYears} ${t('year')} `
-      else if (value !== null && value === 1) return `${value} ${t('year')} `
-      else if (nYears !== null && nYears > 1) return `${nYears} ${t(key)} `
-      else if (nYears === null) return `${value} ${t(key)} `
-    }
-  })
-  gcDate = null
-  return duration
 }
 
 const renderValue = (key, value, lang) => {
   switch (key) {
+    //wheels format
     case "wheelsFrontDimensions":
     case "wheelsBackDimensions":
       return `${value.width}/${value.height} R${value.diameter}`;
 
-    case "mileage":
+    //€ values
     case "b2cMarketValue":
-    case "standardMileage":
-      return parseInt(value).toLocaleString();
-    case "gcDate":
-    case "ownershipDuration":
-      let val
-      if(!gcDate){
-        gcDate = value
-        val = moment(value).format("DD-MM-YYYY");
-      }
-      if(key == 'ownershipDuration') {
-        let res = moment.duration(moment().diff(moment(gcDate)))
-        val = calculateOwnerShipDuration(res)
-        val = val.reverse()
-      }
-      return val 
-    case "nextTechnicalCheckDate":
+      return `${parseInt(value).toLocaleString()} €`;
+
+    //km values
+    case "mileage":
     case "lastServicingKm":
-    case "lastServicingDate": 
-      if(key === "lastServicingKm") return `${parseInt(value).toLocaleString()} km`
-      else return moment(value).format("MM-YYYY");
+    case "standardMileage":
+      return `${parseInt(value).toLocaleString()} ${t("km")}`;
+
+    //date DD-MM-YYYY values
+    case "gcDate":
+      return moment(value).format("DD-MM-YYYY");
+
+    //date MM-YYYY values
+    case "nextTechnicalCheckDate":
+    case "lastServicingDate":
+      return moment(value).format("MM-YYYY");
+
+    //liter display
     case "liter":
       let literStr = "";
       if (lang === "de") {
@@ -85,8 +53,12 @@ const renderValue = (key, value, lang) => {
         literStr = `${Math.ceil(value).toFixed(1)} ${t("unit_liter")}`;
       }
       return literStr;
+
+    //fiscal display
     case "fiscal":
       return `${value} CV`;
+
+    //power display
     case "power":
       let powerOutput = "";
 
@@ -103,13 +75,15 @@ const renderValue = (key, value, lang) => {
       }
 
       return powerOutput;
+
+    //link dispay
     case "purchaseInvoice":
     case "servicingManualPicture":
       return (
         <>
           {t("yes")}{" "}
           <a href={value} target="_blank" rel="noopener noreferrer">
-            {t("download_invoice")}
+            {t(_.camelCase([`download`, key]))}
           </a>
         </>
       );
@@ -125,8 +99,8 @@ const ListTable = ({ items }) => {
       <Row>
         {Object.keys(items).map(
           key =>
-            showableValue(key, items[key], lang) &&
-              key !== 'origin' && <React.Fragment key={key}>
+            showableValue(key, items[key], lang) && (
+              <React.Fragment key={key}>
                 <div className="cell">
                   <div className="item">
                     <div className="label">{t(key)}</div>
@@ -137,7 +111,7 @@ const ListTable = ({ items }) => {
                 </div>
               </React.Fragment>
             )
-          }
+        )}
       </Row>
     </div>
   );
