8a2e8d953d4a4a6f318cf1ec9e2fdd0b7c196700
s.wangchuk <s.wangchuk@autobiz.com>
2020-01-13 16:47:50 +0100

    tourner image

--- src/components/Records/Documents.js
+++ src/components/Records/Documents.js
@@ -1,22 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import pdfIcon from "../../assets/img/ico-pdf.png";
 import { t } from "../common/Translate";
 import Cookies from "js-cookie";
-
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSync } from "@fortawesome/free-solid-svg-icons";
 const Documents = ({ items }) => {
   const lang = Cookies.get("appLanguage");
+  const [popup, setPopup] = useState(false);
+  const [link, setLink] = useState("");
+  let angle = 0;
+
+  const customRotate = () => {
+    let img = document.getElementsByClassName("ril__image");
+    angle = (angle + 90) % 360;
+    img[0].setAttribute("style", `transform: rotate(${angle}deg)`);
+  };
+
   return (
     <div className="list-document">
       {items.map((item, index) => (
         <div className="cell" key={index}>
           <div className="item">
-            <a href={`${item.link}?language=${lang}`} target="_blank" rel="noopener noreferrer">
-              <img src={pdfIcon} alt="pdf" />
-              {t(item.title)}
-            </a>
+            {(item.title === "pdfReport" && (
+              <a
+                href={`${item.link}?language=${lang}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={pdfIcon} alt="pdf" />
+                {t(item.title)}
+              </a>
+            )) || (
+              <a
+                _target="_blank"
+                rel="noopener noreferrer"
+                onClick={() => {
+                  setPopup(true);
+                  setLink(item.link);
+                }}
+              >
+                <img src={pdfIcon} alt="pdf" />
+                {t(item.title)}
+              </a>
+            )}
           </div>
         </div>
       ))}
+
+      {popup && link && (
+        <Lightbox
+          mainSrc={link}
+          toolbarButtons={[
+            <FontAwesomeIcon
+              className="ril__toolbarItemChild ril__builtinButton"
+              icon={faSync}
+              onClick={() => customRotate()}
+            />
+          ]}
+          onCloseRequest={() => setPopup(false)}
+        />
+      )}
     </div>
   );
 };
