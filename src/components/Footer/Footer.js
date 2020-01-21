6df4fc054a794b1ff521e7c7ae4818d979bff971
s.wangchuk <s.wangchuk@autobiz.com>
2020-01-21 18:09:13 +0100

    ajouter le lien terms of sales

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Translate from "../common/Translate";
 import { Link } from "react-router-dom";
-import { contactEmail, StaticFile } from "../../config"
+import { contactEmail, StaticFile, staticContentUrl } from "../../config"
 import Cookies from "js-cookie";
 
 const Footer = () => {
@@ -10,6 +10,9 @@ const Footer = () => {
   return (
     <footer>
       <ul>
+        <li>
+          <a href={`${staticContentUrl}/${lang}/terms_of_sales.pdf`} target="__blank"><Translate code="terms_of_sales" /></a>
+        </li>
         <li>
           <a href={`${StaticFile}${lang.toUpperCase()}/ConditionsGenerales.pdf`} target="__blank"><Translate code="terms_of_use" /></a>
         </li>
