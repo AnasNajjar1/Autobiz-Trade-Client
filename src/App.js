bae4b45cf6c66e3b03adf3484671629a25cce625
o.medfai <o.medfai@autobiz.com>
2021-06-16 09:44:09 +0100

    new translate package add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./assets/scss/app.scss";
 import "./assets/scss/report.scss";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import { LanguageContext, dictionnary } from "./language-context";
+import { dictionnary } from "./language-context";
 import LoginView from "./views/LoginView.js";
 import RecordsView from "./views/RecordsView.js";
 import RecordsListView from "./views/RecordsListView.js";
@@ -17,24 +17,24 @@ import awsconfig from "./aws-config";
 import AuthRequiredRoute from "./components/Login/AuthRequiredRoute";
 import _ from "lodash";
 import Cookies from "js-cookie";
-import getTranslations from "./translations/services/getTranslations";
-import cacheStaticContent from "./translations/services/cacheStaticContent";
 import TagManager from "react-gtm-module";
 import { tagManagerArgs, didomiConfig, zendeskConfig } from "./config";
 import RegisterView from "./views/RegisterView";
 import { DidomiSDK } from "@didomi/react";
 import Zendesk from "react-zendesk";
+import { TranslateProvider, TranslateInstance } from "autobiz-translate";
 
 Amplify.configure(awsconfig);
 TagManager.initialize(tagManagerArgs);
 
 class App extends Component {
-  state = {
-    language: dictionnary[process.env.REACT_APP_LANG],
-  };
-
   constructor() {
     super();
+
+    this.state = {
+      language: process.env.REACT_APP_LANG,
+      stage: process.env.REACT_APP_ENV === "prod" ? "prod" : "dev",
+    };
     this.entryPath = {
       pathname: "/records",
     };
@@ -47,18 +47,16 @@ class App extends Component {
   }
 
   changeLanguage = async (language) => {
-    if (dictionnary.hasOwnProperty(language)) {
-      try {
-        const languageDict = await getTranslations(language);
-        moment.locale(language);
-        this.setState({ language: languageDict }); //dictionnary[language]
-      } catch (e) {
-        console.log("missing trads", e.toString());
-      }
-      return;
-    }
-    console.log("language not found : ", language);
-    return;
+    moment.locale(language);
+    localStorage.removeItem("translation");
+    const translation = await TranslateInstance.init(
+      "trade-app",
+      language,
+      this.state.stage
+    );
+    if (!translation)
+      TranslateInstance.appendCustomDictionary(dictionnary[language]);
+    this.setState({ language });
   };
 
   handleChangeLanguage = async (event) => {
@@ -74,21 +72,14 @@ class App extends Component {
       language = Cookies.get("appLanguage");
     } else {
       let res = _.split(window.navigator.language, "-", 1);
-      if (dictionnary.hasOwnProperty(res[0])) {
-        language = res[0];
-        Cookies.set("appLanguage", language, { expires: 365 });
-      } else {
-        Cookies.set("appLanguage", process.env.REACT_APP_LANG, {
-          expires: 365,
-        });
-      }
+      if (dictionnary.hasOwnProperty(res[0])) language = res[0];
+      Cookies.set("appLanguage", language, { expires: 365 });
     }
 
     this.changeLanguage(language);
 
     window.addEventListener("changeLanguage", this.handleChangeLanguage);
     window.addEventListener("storage", this.handleRefresh);
-    Object.keys(dictionnary).map((language) => cacheStaticContent(language));
   }
 
   handleRefresh = async (e) => {
@@ -103,10 +94,15 @@ class App extends Component {
   }
 
   render() {
+    const { language, stage } = this.state;
     return (
       <>
         <BrowserRouter>
-          <LanguageContext.Provider value={this.state.language}>
+          <TranslateProvider
+            projectName="trade-app"
+            stage={stage}
+            language={language}
+          >
             <QueryParamProvider ReactRouterRoute={Route}>
               <Switch>
                 <Route path="/reports/:lang/:refId" component={ReportsView} />
@@ -130,7 +126,11 @@ class App extends Component {
                 <Route
                   path="/login"
                   render={(props) => (
-                    <LoginView {...props} entryPath={this.entryPath} didomi={this.didomiObject} />
+                    <LoginView
+                      {...props}
+                      entryPath={this.entryPath}
+                      didomi={this.didomiObject}
+                    />
                   )}
                 />
                 <Route
@@ -142,7 +142,7 @@ class App extends Component {
                 <Redirect from="/" exact to="/records" />
               </Switch>
             </QueryParamProvider>
-          </LanguageContext.Provider>
+          </TranslateProvider>
         </BrowserRouter>
         <DidomiSDK
           apiKey={didomiConfig.apiKey}
